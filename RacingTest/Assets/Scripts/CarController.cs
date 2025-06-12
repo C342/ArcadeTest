2c71b96b4aaa88a1ee4b8ffc6e1d8e1583d64f70
C342 <mbuzurin2024@elam.co.uk>
2025-06-12 11:59:55 +0100

    g

--- RacingTest/Assets/Scripts/CarController.cs
+++ RacingTest/Assets/Scripts/CarController.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class car : MonoBehaviour
 {
@@ -9,20 +11,34 @@ public class car : MonoBehaviour
     public float drivespeed, steerspeed;
     float horizontalInput, verticalInput;
 
+    public TextMeshProUGUI speedText;
+
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
+
+        DisplaySpeed();
     }
 
     void FixedUpdate()
     {
-        float motor = Input.GetAxis("Vertical") * drivespeed;
+        float motor = verticalInput * drivespeed;
+
         wheel1.motorTorque = motor;
         wheel2.motorTorque = motor;
         wheel3.motorTorque = motor;
         wheel4.motorTorque = motor;
+
         wheel1.steerAngle = steerspeed * horizontalInput;
         wheel2.steerAngle = steerspeed * horizontalInput;
+    }
+
+    void DisplaySpeed()
+    {
+        float speed = rigid.velocity.magnitude;
+        float speedMph = speed * 1.6f;
+
+        speedText.text = "" + Mathf.RoundToInt(speedMph).ToString() + "MPH";
     }
 }
