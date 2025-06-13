06298d90946628516d9f43fbd086f13092f887be
C342 <mbuzurin2024@elam.co.uk>
2025-06-13 12:45:32 +0100

    h

--- RacingTest/Assets/Scripts/SceneController.cs
+++ RacingTest/Assets/Scripts/SceneController.cs
@@ -1,16 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneController : MonoBehaviour
 {
+    public GameObject Car;
+    public TextMeshProUGUI RestartText;
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.R))
         {
             SceneManager.LoadScene("Level");
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (Car != null)
+            {
+                float currentX = Car.transform.eulerAngles.x;
+                float currentZ = Car.transform.eulerAngles.z;
+                Car.transform.rotation = Quaternion.Euler(currentX, 180f, currentZ);
+            }
+        }
+
+        if (Car != null && RestartText != null)
+        {
+            if (Car.transform.up.y < 0.1f)
+            {
+                RestartText.gameObject.SetActive(true);
+            }
+            else
+            {
+
+                RestartText.gameObject.SetActive(false);
+            }
         }
     }
 }
