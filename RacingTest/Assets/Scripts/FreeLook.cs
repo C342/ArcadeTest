1d989de685cd164000870c60b9adbb3c20b57877
C342 <mbuzurin2024@elam.co.uk>
2025-06-17 11:50:31 +0100

    h

---
+++ RacingTest/Assets/Scripts/FreeLook.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreeLook : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+    public float resetSpeed = 5f;
+
+    private float yRotation = 0f;
+    private Quaternion initialRotation;
+    private bool isResetting = false;
+
+    void Start()
+    {
+        initialRotation = transform.localRotation;
+        yRotation = transform.localEulerAngles.y;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(1) && !isResetting)
+        {
+            float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+            yRotation += mouseX;
+
+            transform.localRotation = Quaternion.Euler(0f, yRotation, 0f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            isResetting = true;
+        }
+
+        if (isResetting)
+        {
+            transform.localRotation = Quaternion.Slerp(transform.localRotation, initialRotation, Time.deltaTime * resetSpeed);
+
+            if (Quaternion.Angle(transform.localRotation, initialRotation) < 0.1f)
+            {
+                transform.localRotation = initialRotation;
+                yRotation = initialRotation.eulerAngles.y;
+                isResetting = false;
+            }
+        }
+    }
+}
