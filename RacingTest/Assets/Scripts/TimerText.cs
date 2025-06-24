c1662de0f3c99a6434ae2dd60b2320a30f92471f
C342 <mbuzurin2024@elam.co.uk>
2025-06-24 15:38:16 +0100

    h

--- RacingTest/Assets/Scripts/TimerText.cs
+++ RacingTest/Assets/Scripts/TimerText.cs
@@ -7,30 +7,5 @@ using UnityEngine;
 
 public class TimerText : MonoBehaviour
 {
-    private float TimerText;
 
-    [SerializeField] private Text stopwatchText;
-
-    void Start()
-    {
-        timer = Time.time;
-    }
-
-    void Update()
-    {
-        float elapsed = Time.time - startTime;
-
-        int minutes = Mathf.FloorToInt(elapsed / 60);
-        int seconds = Mathf.FloorToInt(elapsed % 60);
-        int milliseconds = Mathf.FloorToInt((elapsed * 100) % 100);
-
-        if (stopwatchText != null)
-        {
-            stopwatchText.text = $"{minutes:D2}:{seconds:D2}.{milliseconds:D2}";
-        }
-        else
-        {
-            Debug.Log($"{minutes:D2}:{seconds:D2}.{milliseconds:D2}");
-        }
-    }
 }
